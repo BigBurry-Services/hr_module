b0ea9c75fcc6f1046cb6633ec686fa5fc877e1cd
widecity <amal@bigburry.com>
2025-12-04 16:05:03 +0530

    Update hr.bat

--- hr.bat
+++ hr.bat
@@ -8,7 +8,7 @@ echo ==== Django Task Started at %date% %time% ==== >> C:\HrModule\django_log.tx
 call "E:\hrmodule\hr_module\env\Scripts\activate.bat" >> C:\HrModule\django_log.txt 2>&1
 
 :: Go to the Django project directory
-cd /d "E:\hrmodule\hr_module\hr_module" >> C:\HrModule\django_log.txt 2>&1
+cd /d "E:\hrmodule\hr_module" >> C:\HrModule\django_log.txt 2>&1
 
 :: Run server (or your command)
 python manage.py runserver 0.0.0.0:80 >> C:\HrModule\django_log.txt 2>&1
